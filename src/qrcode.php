0a462f803db9f9f58423d594ce1074ad782a8324
FerreWagner <18408229270@163.com>
2018-08-09 20:25:48 +0800

    php qrcode success

--- src/qrcode.php
+++ src/qrcode.php
@@ -5,8 +5,14 @@
  * Date: 2018/8/9
  * Time: 20:11
  */
+/**
+ * 在http://phpqrcode.sourceforge.net/下载qrcode
+ */
+include "phpqrcode/qrlib.php";
 
-class qrForPhp
-{
-    
-}
+//QRcode::png('fuck');
+//QRcode::png('fuck', 'file.jpg');//文件输出
+//QRcode::png('fuck', false, QR_ECLEVEL_L);//容错级别
+//QRcode::png('fuck', false, QR_ECLEVEL_L, 10);//尺寸
+//QRcode::png('fuck', false, QR_ECLEVEL_L, 10, 3);//边距
+QRcode::png('fuck', 'ferre.jpg', QR_ECLEVEL_L, 10, 3, true);//保存且打印
